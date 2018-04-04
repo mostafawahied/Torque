25db084a394f44ce87d55cae6083d879378ff597
Mostafa Wahied <37817812+mostafawahied@users.noreply.github.com>
2018-04-04 15:39:50 +0200

    enviroment

---
+++ Torque/Assets/Scripts/Main menu/OptionMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionMenu : MonoBehaviour {
+
+    public AudioMixer audiomixer;
+        
+    public void Option(float volume)
+    {
+        audiomixer.SetFloat("volume",volume);
+    }
+}
